b70152a583abb9b9c84b970bb513b6c4b96ead3a
ILAroy-611 <63605660+ILAroy-611@users.noreply.github.com>
2020-04-17 19:37:45 +0530

    Add files via upload

---
+++ ass-16.py
@@ -0,0 +1,148 @@
+'''
+Created on 15-Apr-2020
+
+@author: ila roy
+'''
+#DSA-Assgn-16
+class Queue:
+    def __init__(self,max_size):
+
+        self.__max_size=max_size
+        self.__elements=[None]*self.__max_size
+        self.__rear=-1
+        self.__front=0
+
+    def is_full(self):
+        if(self.__rear==self.__max_size-1):
+                return True
+        return False
+
+    def is_empty(self):
+        if(self.__front>self.__rear):
+            return True
+        return False
+
+    def enqueue(self,data):
+        if(self.is_full()):
+            print("Queue is full!!!")
+        else:
+            self.__rear+=1
+            self.__elements[self.__rear]=data
+
+    def dequeue(self):
+        if(self.is_empty()):
+            print("Queue is empty!!!")
+        else:
+            data=self.__elements[self.__front]
+            self.__front+=1
+            return data
+
+    def display(self):
+        for index in range(self.__front, self.__rear+1):
+            print(self.__elements[index])
+
+
+    def get_max_size(self):
+        return self.__max_size
+
+    #You can use the below __str__() to print the elements of the DS object while debugging
+    def __str__(self):
+        msg=[]
+        index=self.__front
+        while(index<=self.__rear):
+            msg.append((str)(self.__elements[index]))
+            index+=1
+        msg=" ".join(msg)
+        msg="Queue data(Front to Rear): "+msg
+        return msg
+
+class Stack:
+    def __init__(self,max_size):
+        self.__max_size=max_size
+        self.__elements=[None]*self.__max_size
+        self.__top=-1
+
+    def is_full(self):
+        if(self.__top==self.__max_size-1):
+            return True
+        return False
+
+    def is_empty(self):
+        if(self.__top==-1):
+            return True
+        return False
+
+    def push(self,data):
+        if(self.is_full()):
+            print("The stack is full!!")
+        else:
+            self.__top+=1
+            self.__elements[self.__top]=data
+
+    def pop(self):
+        if(self.is_empty()):
+            print("The stack is empty!!")
+        else:
+            data= self.__elements[self.__top]
+            self.__top-=1
+            return data
+
+    def display(self):
+        if(self.is_empty()):
+            print("The stack is empty")
+        else:
+            index=self.__top
+            while(index>=0):
+                print(self.__elements[index])
+                index-=1
+
+    def get_max_size(self):
+        return self.__max_size
+
+    #You can use the below __str__() to print the elements of the DS object while debugging
+    def __str__(self):
+        msg=[]
+        index=self.__top
+        while(index>=0):
+            msg.append((str)(self.__elements[index]))
+            index-=1
+        msg=" ".join(msg)
+        msg="Stack data(Top to Bottom): "+msg
+        return msg
+
+def separate_boxes(box_stack):
+    #Remove pass and write your logic here
+    result=Queue(box_stack.get_max_size())
+    count=box_stack.get_max_size()
+    stack_color=Stack(box_stack.get_max_size())
+    while count>0:
+        color=box_stack.pop()
+        if color!="Red" and color!="Green" and color!="Blue":
+            result.enqueue(color)
+            count=count-1
+        else:
+            stack_color.push(color)
+            count=count-1
+    while not stack_color.is_empty():
+        box_stack.push(stack_color.pop())
+    return result
+
+#Use different values for stack and test your program
+box_stack=Stack(8)
+box_stack.push("Red")
+box_stack.push("Magenta")
+box_stack.push("Yellow")
+box_stack.push("Red")
+box_stack.push("Orange")
+box_stack.push("Green")
+box_stack.push("White")
+box_stack.push("Purple")
+print("Boxes in the stack:")
+box_stack.display()
+result=separate_boxes(box_stack)
+print()
+print("Boxes in the stack after modification:")
+box_stack.display()
+print("Boxes in the queue:")
+result.display()
+                                                    
